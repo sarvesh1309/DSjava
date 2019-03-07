153d4231bf27acc705dbca664ec3998863ea9ed7
sarvesh_kumar <sarvesh_kumar@apple.com>
2019-03-07 01:53:31 -0800

    graph codes

--- src/com/practice/graph/DetectCycleInGraph.java
+++ src/com/practice/graph/DetectCycleInGraph.java
@@ -5,6 +5,11 @@ import java.util.Map;
 
 public class DetectCycleInGraph {
 
+	/**
+	 * detect cycle using DFS
+	 * @param g
+	 * @return
+	 */
 	public boolean isCyclicGraphDFS(int[][] g) {
 		int visited[] = new int[g.length];
 		for (int i = 0; i < g.length; i++) {
@@ -21,6 +26,14 @@ public class DetectCycleInGraph {
 		return false;
 	}
 
+	/**
+	 * DFS traversal to detect cycle in graph
+	 * @param g
+	 * @param start
+	 * @param visited
+	 * @param parent
+	 * @return
+	 */
 	private boolean DFS(int[][] g, int start, int[] visited, Map<Integer, Integer> parent) {
 		visited[start] = 1;
 		for (int i = 0; (i < g.length); i++) {
@@ -29,6 +42,8 @@ public class DetectCycleInGraph {
 					parent.put(i, start);
 					return DFS(g, i, visited, parent);
 				}
+				// if there is exist a edge from u to v and v is not immediate parent in DFS tree then it is a back edge 
+				//the back edge thus confirms cycle in graph
 				if (!(parent.get(start) == i)) {
 					return true;
 				}
